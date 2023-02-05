b5719023e1f9682e8e640fa2e2cba7f99201196b
Joao <Joao@HP-OMEN-4600>
2023-02-05 12:18:35 +0000

    local

---
+++ src/main/java/turma11o/coisasparagithub/CoisasParaGitHub.java
@@ -0,0 +1,12 @@
+package turma11o.coisasparagithub;
+
+/**
+ *
+ * @author Joao Barbosa
+ */
+public class CoisasParaGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
